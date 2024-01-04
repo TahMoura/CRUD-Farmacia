ee1934f7da30b67f09a04d16ad604c825dd37fbe
TaliaMoura <taliamouragit@outlook.com>
2024-01-04 15:28:18 -0300

    Implementação da classe ProdutoRepository na camada repository

---
+++ src/main/java/br/com/crudfarmacia/repository/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package br.com.crudfarmacia.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import br.com.crudfarmacia.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository <Produto, Long> {
+    List<Produto> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+}
