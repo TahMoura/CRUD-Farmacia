be0f12e2ca3ba451e2e26ad5b7357f958a98ed74
TaliaMoura <taliamouragit@outlook.com>
2024-01-04 12:06:30 -0300

    Configurando o Projeto

---
+++ src/test/java/br/com/crudfarmacia/CrudfarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crudfarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudfarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
